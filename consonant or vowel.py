c2e34f1c975ff80122d3c6dd712473a2030a5ffe
Rahini8 <36723038+Rahini8@users.noreply.github.com>
2018-02-23 10:44:06 +0530

    Rename vowel or consonant.py to consonant or vowel.py

---
+++ consonant or vowel.py
@@ -0,0 +1,7 @@
+l=input("Input a letter of the alphebet:")
+if l in('a','e','i','o','u'):
+  print("%s is vowel."%l)
+elif l == 'y':
+    print("sometimes letter y stands for vowel,sometimes stand for consonant.")
+else:
+print("%s is a consonant." %l)
