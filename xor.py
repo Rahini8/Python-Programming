db69303f0c98b4581cce66f40cc90390a0bf15e1
Rahini8 <36723038+Rahini8@users.noreply.github.com>
2018-12-19 15:57:09 +0530

    Create xor.py

---
+++ xor.py
@@ -0,0 +1,27 @@
+n=int(input())
+
+q=int(input())
+
+li=[]
+
+b=[]
+
+for i in range(0,n):
+    
+    li.append(int(input()))
+
+    for i in range(0,q):
+    
+    s=0
+    
+    u=int(input())
+    
+    v=int(input())
+    
+    for i in range(u-1,v):
+        
+        s=s^li[i]
+    
+    b.append(s)
+
+print(b)
